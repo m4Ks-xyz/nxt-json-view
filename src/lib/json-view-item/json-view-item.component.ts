c01f438e9272066fcfc39b8d901375f058b9f5f4
Maks <maksymiliantomowiak321@gmail.com>
2025-04-27 21:13:23 +0200

    feat: computed signals

--- src/lib/json-view-item/json-view-item.component.ts
+++ src/lib/json-view-item/json-view-item.component.ts
@@ -2,8 +2,7 @@ import { NgClass } from '@angular/common';
 import {
   ChangeDetectionStrategy,
   Component,
-  OnInit,
-  effect,
+  computed,
   input,
   signal,
 } from '@angular/core';
@@ -25,16 +24,7 @@ import {
   imports: [NgClass],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class JsonViewItemComponent implements OnInit {
-  constructor() {
-    effect(() => {
-      this.data();
-      if (this.isInit()) {
-        this.init();
-      }
-    });
-  }
-
+export class JsonViewItemComponent {
   /** JSON data, any valid JSON object */
   readonly data = input<any>();
   readonly key = input<string>();
@@ -43,82 +33,102 @@ export class JsonViewItemComponent implements OnInit {
   readonly levelLabels = input<LevelLabels>();
 
   isOpen = signal<boolean>(false);
-  childrenKeys = signal<string[] | undefined>(undefined);
-  hasChildren = signal<boolean>(false);
-  dataType = signal<string | undefined>(undefined);
-  value = signal<any>(undefined);
-  valueType = signal<string | undefined>(undefined);
-  isObject = signal<boolean>(false);
-  isArray = signal<boolean>(false);
-  isInit = signal<boolean>(false);
-  _levelLabels = signal<{ [key: string]: string }>({});
 
-  ngOnInit() {
-    this.init();
-    this.isInit.set(true);
-  }
-
-  init() {
-    this.levelLabelHandle();
-    this.levelOpenHandle();
-    this.childrenKeysHandle();
-    this.dataHandle();
-  }
-
-  levelLabelHandle() {
+  readonly _levelLabels = computed(() => {
     const levelLabels = this.levelLabels();
     if (levelLabels !== undefined) {
-      this._levelLabels.set(levelLabels[this.level()] || {});
+      return levelLabels[this.level()] || {};
     }
-  }
+    return {};
+  });
 
-  levelOpenHandle() {
+  readonly _levelOpen = computed(() => {
     const levelOpen = this.levelOpen();
-    if (!isUndefined(levelOpen) && this.level() <= levelOpen) {
-      this.isOpen.set(true);
+    if (isUndefined(levelOpen)) {
+      return undefined;
     }
-  }
+    return levelOpen > 0 ? levelOpen : undefined;
+  });
 
-  childrenKeysHandle() {
+  readonly _hasChildren = computed(() => {
     const data = this.data();
     if (isObject(data)) {
-      this.childrenKeys.set(Object.keys(data));
-      if (this.childrenKeys() && this.childrenKeys()!.length) {
-        this.hasChildren.set(true);
+      return this._childrenKeys() && this._childrenKeys()!.length > 0;
+    }
+    return false;
+  });
+
+  readonly _childrenKeys = computed(() => {
+    const data = this.data();
+    return isObject(data) ? Object.keys(data) : undefined;
+  });
+
+  readonly valueType = computed(() => {
+    const data = this.data();
+    if (isObject(data)) {
+      if (isArray(data)) {
+        return 'Array';
+      } else {
+        return 'Object';
+      }
+    } else {
+      if (isString(data)) {
+        return 'string';
+      } else if (isNumber(data)) {
+        return 'number';
+      } else if (isBoolean(data)) {
+        return 'boolean';
+      } else if (null === data) {
+        return 'null';
       }
     }
-  }
+    return 'null';
+  });
 
-  dataHandle() {
+  readonly dataType = computed(() => {
     const data = this.data();
+
     if (isObject(data)) {
-      this.isObject.set(true);
-      this.dataType.set('Object');
       if (isArray(data)) {
-        this.isArray.set(true);
-        this.dataType.set('Array');
+        return 'Array';
       }
       const key = this.key();
       if (key && this._levelLabels()[key]) {
-        this.dataType.set(this._levelLabels()[key]);
+        return this._levelLabels()[key];
       }
-    } else {
-      this.value.set(data);
-      if (isString(data)) {
-        this.valueType.set('string');
-      } else if (isNumber(data)) {
-        this.valueType.set('number');
-      } else if (isBoolean(data)) {
-        this.valueType.set('boolean');
-      } else if (null === data) {
-        this.valueType.set('null');
-        this.value.set('null');
-      }
+      return 'Object';
     }
-  }
+    return 'null';
+  });
+
+  readonly isObject = computed(() => {
+    const data = this.data();
+    if (isObject(data)) {
+      return true;
+    }
+    return false;
+  });
+
+  readonly isArray = computed(() => {
+    const data = this.data();
+    if (isArray(data)) {
+      return true;
+    }
+    return false;
+  });
+
+  readonly value = computed(() => {
+    const data = this.data();
+
+    if (data === null) {
+      return 'null';
+    } else {
+      return data;
+    }
+  });
 
   toggle() {
-    if (!(this.childrenKeys() && this.childrenKeys()!.length)) {
+    if (!(this._childrenKeys() && this._childrenKeys()!.length)) {
       return;
     }
     this.isOpen.set(!this.isOpen());
